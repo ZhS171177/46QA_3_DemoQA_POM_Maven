356997664bd1c8fe4905a3939ef79192e9c95406
ZhS171177 <zhanna-schmidt@web.de>
2024-10-24 12:21:54 +0300

    add new file

--- src/main/java/demoqa/core/ApplicationManager.java
+++ src/main/java/demoqa/core/ApplicationManager.java
@@ -1,9 +1,11 @@
 package demoqa.core;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class ApplicationManager {
@@ -12,7 +14,22 @@ public class ApplicationManager {
     public WebDriverWait wait;
 
     protected void init() {
-        driver = new ChromeDriver();
+        String browser = System.getProperty("browser", "chrome");
+// Проверяет значение переменной browser и в зависимости от результата инициализирует нужный драйвер
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default: // Это резервный сценарий на случай, если значение browser не совпадает ни с одним из указанных случаев
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+        }
+
+        //driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // неявное
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));// явное
@@ -24,4 +41,7 @@ public class ApplicationManager {
             driver.quit(); // Остановка драйвера после всех тестов
         }
     }
+
+
+
 }
